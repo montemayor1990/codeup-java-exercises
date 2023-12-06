22948f138b5f2979272b2da4da9f2cff412171d6
David Montemayor <montemayor1990@gmail.com>
2023-12-06 11:47:52 -0600

    hello_world

---
+++ src/HelloWorld.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, world!");
+//        String myName = "Jeremy";
+//        System.out.println("My name is " + myName);
+//        char dollarsSign = '$';
+//        System.out.println(dollarsSign);
+//        System.out.print("Hello, world!");
+
+//        int myNum = 123;
+//        long myLong = 1234;
+//        float myFloat = 3.14f;
+//        double myDouble = 123.56;
+//
+//
+
+        int myFavoriteNumber = 17;
+        String myString = "David";
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+
+        float myNumber = 123;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+
+
+    }
+}
